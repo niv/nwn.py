ac7535b47a36cb75b0b14a35ace8c8fc01aed95c
Bernhard Stoeckner <n@e-ix.net>
2025-03-22 19:14:33 +0100

    doc improvement

--- src/nwn/__init__.py
+++ src/nwn/__init__.py
@@ -1,4 +1,26 @@
 """
-This package provides tools for working with Neverwinter Nights data files
-and miscellaneous engine utilities.
+nwn
+===
+
+A python package with various modules for accessing Neverwinter Nights: Enhanced Edition
+data formats and functionality.
+
+Stability
+---------
+
+This package is currently in ALPHA state. API stability is not guaranteed.
+
+Installation
+------------
+
+The package is available on PyPI and can be installed with pip:
+
+.. code-block:: bash
+
+    pip install nwn
+
+License
+-------
+
+This package is licensed under the MIT license.
 """
